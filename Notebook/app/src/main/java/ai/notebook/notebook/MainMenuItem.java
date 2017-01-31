9ed8c7a2b9fc650b9d0047af50ea925ad7070649
James Pierce <james.pierce82@stclairconnect.ca>
2017-01-31 13:46:35 -0500

    Started adding Menu Item stuff

---
+++ Notebook/app/src/main/java/ai/notebook/notebook/MainMenuItem.java
@@ -0,0 +1,13 @@
+package ai.notebook.notebook;
+
+import android.media.Image;
+
+/**
+ * Created by web on 2017-01-31.
+ */
+
+public class MainMenuItem {
+
+    private String itemName;
+    private int headerImage;
+}
