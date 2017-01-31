bdf32be13a25785844608efa47d86e805b1cd9e8
web <web@amso0055z-16.local>
2017-01-31 13:20:54 -0500

    Added a MainList file
    * File is for the RecyclerView class

---
+++ Notebook/app/src/main/java/ai/notebook/notebook/MainList.java
@@ -0,0 +1,6 @@
+package ai.notebook.notebook;
+
+// Utilizing a guide found on https://guides.codepath.com/android/using-the-recyclerview for RecyclerView
+// Need a reycler view because of the content in the list
+
+// Will need to fill this out eventually, this will hold the recycler view
