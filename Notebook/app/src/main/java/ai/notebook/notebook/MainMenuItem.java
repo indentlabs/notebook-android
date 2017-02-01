8eabe25600c263dc3bc6f7e0849eed1a383dd6cc
James Pierce <james.pierce82@stclairconnect.ca>
2017-02-01 09:33:41 -0500

    Added content to MainMenuItem
    * Added getters and setters
    * Added construtor
    * Added toString

--- Notebook/app/src/main/java/ai/notebook/notebook/MainMenuItem.java
+++ Notebook/app/src/main/java/ai/notebook/notebook/MainMenuItem.java
@@ -1,6 +1,5 @@
 package ai.notebook.notebook;
 
-import android.media.Image;
 
 /**
  * Created by web on 2017-01-31.
@@ -10,4 +9,30 @@ public class MainMenuItem {
 
     private String itemName;
     private int headerImage;
+
+    public MainMenuItem(String itemName, int headerImage) {
+        this.itemName = itemName;
+        this.headerImage = headerImage;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getHeaderImage() {
+        return headerImage;
+    }
+
+    public void setHeaderImage(int headerImage) {
+        this.headerImage = headerImage;
+    }
+
+    @Override
+    public String toString() {
+        return getItemName();
+    }
 }
